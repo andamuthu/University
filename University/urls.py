aa47a383b678a90668406db951f3e607be923f08
andamuthu <andamuthu.a@gmail.com>
2020-04-23 21:01:42 +0530

    First commit

---
+++ University/urls.py
@@ -0,0 +1,23 @@
+"""University URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+app_name = 'students_details'
+urlpatterns = [
+                 # path('one/', include('one.urls')),
+    path('students_details/', include('students_details.urls')),
+    path('admin/', admin.site.urls),
+]
